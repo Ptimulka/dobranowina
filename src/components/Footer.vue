c174262b490542bb05960c5281b2bb8a7c00a1b6
Marta <ptimuleczka@gmail.com>
2020-04-13 21:29:20 +0200

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-footer class="primary">
+    <v-container>
+    <v-row justify="center">
+      <v-col  class="text-center" cols="12" xs="12" md="4">
+        <v-btn text href="https://www.facebook.com/dobranowinanet/"><v-icon size="24px">mdi-facebook</v-icon>DobraNowina.net</v-btn>
+      </v-col>
+      <v-col class="text-center" cols="12" xs="12" md="4">
+        <v-btn text href="https://www.youtube.com/user/dobranowinanet/featured"><v-icon size="24px">mdi-youtube</v-icon>DobraNowina.net</v-btn>
+      </v-col>
+      <v-col class="text-center" cols="12" xs="12" md="4">
+        <v-btn text href="https://www.youtube.com/user/KapustaShow/featured"><v-icon size="24px">mdi-youtube</v-icon>Mikołaj Kapusta</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+  </v-footer>
+</template>
