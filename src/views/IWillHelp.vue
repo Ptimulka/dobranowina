486cac733fa443f210c7215f9da54629b12989a3
Marta <ptimuleczka@gmail.com>
2020-05-24 13:19:03 +0200

    Better list of left livestreams on IWillHelp page

--- src/views/IWillHelp.vue
+++ src/views/IWillHelp.vue
@@ -6,13 +6,30 @@
 
       <h1 class="py-5 title text-center">1. Spisywanie pytań z livestreamów</h1>
       <h3 class="py-5 px-5 subtitle-2 text-center">Możesz obejrzeć jeden z livestreamów Mikołaja i spisać pytania na które on odpowiada. Najlepiej żebyś spisał także jego odpowiedzi, przynajmniej  w skróconej formie, ale jeśli nie masz dużo czasu to wystarczą same pytania.</h3>
-      <h3 class="py-5 px-5 subtitle-2 text-center">Lista linków do livestreamów, które jeszcze nie zostały spisane:</h3>
-      <template v-for="livestream in todoLivestreams">
-        <h4 class="pt-5 subtitle-2 text-center" :key="livestream.dateread">{{ livestream.dateread }}</h4>
-        <h4 class="pb-5 subtitle-2 text-center" :key="livestream.link"><a :href="livestream.link">{{ livestream.link }}</a></h4>
-      </template>
+
+      <v-row no-gutters justify="center">
+        <v-col cols="12" xs="12" sm="6">
+          <v-expansion-panels>
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <h2 class="subtitle-2 center">Lista linków do livestreamów, które nie zostały spisane:</h2>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <template v-for="livestream in todoLivestreams">
+                  <h4 class="pt-5 subtitle-2 text-center" :key="livestream.dateread">{{ livestream.dateread }}</h4>
+                  <h4 class="pb-5 subtitle-2 text-center" :key="livestream.link"><a :href="livestream.link">{{ livestream.link }}</a></h4>
+                </template>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+        </v-col>
+      </v-row>
+
       <h1 class="py-5 title text-center">2. Poprawianie błędów</h1>
       <h3 class="py-5 px-5 subtitle-2 text-center">Widzisz jakiś błąd, literówkę, nieścisłość? Zgłoś to!</h3>
+
+      <h1 class="py-5 title text-center">3. Dodawanie linków czasowych</h1>
+      <h3 class="py-5 px-5 subtitle-2 text-center">Obecnie większość pytań ma linki tylko do całych livestreamów, a przydałyby się linki do konretnych punktów czasowych.</h3>
 
     </v-card>
   </v-container>
