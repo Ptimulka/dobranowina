1e38cb1b7db2184e3773443ec9a5211f534f3157
Marta <ptimuleczka@gmail.com>
2021-06-15 17:56:48 +0200

    Add github link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,14 +2,17 @@
   <v-footer class="primary">
     <v-container>
     <v-row justify="center">
-      <v-col  class="text-center" cols="12" xs="12" md="4">
+      <v-col  class="text-center" cols="12" xs="12" md="3">
         <v-btn text href="https://www.facebook.com/dobranowinanet/"><v-icon size="24px">mdi-facebook</v-icon>DobraNowina.net</v-btn>
       </v-col>
-      <v-col class="text-center" cols="12" xs="12" md="4">
+      <v-col class="text-center" cols="12" xs="12" md="3">
         <v-btn text href="https://www.youtube.com/user/dobranowinanet/featured"><v-icon size="24px">mdi-youtube</v-icon>DobraNowina.net</v-btn>
       </v-col>
-      <v-col class="text-center" cols="12" xs="12" md="4">
+      <v-col class="text-center" cols="12" xs="12" md="3">
         <v-btn text href="https://www.youtube.com/user/KapustaShow/featured"><v-icon size="24px">mdi-youtube</v-icon>Mikołaj Kapusta</v-btn>
+      </v-col>
+      <v-col class="text-center" cols="12" xs="12" md="3">
+        <v-btn text href="https://github.com/Ptimulka/dobranowina"><v-icon size="24px">mdi-github-circle</v-icon>dobranowina</v-btn>
       </v-col>
     </v-row>
   </v-container>
